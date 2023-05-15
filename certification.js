9ef9f83ed441bb22d4cbc8ec63f3d244720a3925
IngReymonPM <133682315+IngReymonPM@users.noreply.github.com>
2023-05-15 11:03:42 -0600

    Create certification.js

---
+++ certification.js
@@ -0,0 +1,47 @@
+// This is an in-line comment.
+/* This is a
+multi-line comment */
+var myName;
+// Configuración
+var a;
+// instrucciones a debe ser igual a 5.
+// Aprobado:b debe ser igual a -6.
+// Aprobado:c debe ser igual a 2.
+// Aprobado:Debes usar el operador -= para cada variable.
+// Aprobado:No debes modificar el código sobre el comentario especificado.
+// Cambia solo el código debajo de esta línea
+var a = 7;
+let a = 11;
+let b = 9;
+let c = 3;
+
+// Cambia solo el código debajo de esta línea
+a = a -= 6;
+b = b -= 15;
+c = c -= 1;
+//a debe ser igual a 25.
+//Aprobado:b debe ser igual a 36.
+//Aprobado:c debe ser igual a 46.
+//Aprobado:Debes usar el operador *= para cada variable.
+//Aprobado:No debes modificar el código sobre el comentario especificado.
+let a = 5;
+let b = 12;
+let c = 4.6;
+
+// Cambia solo el código debajo de esta línea
+a *= 5;
+b *= 3;
+c *= 10;
+//Aprobado:a debe ser igual a 4.
+//Aprobado:b debe ser igual a 27.
+//Aprobado:c debe ser igual a 3.
+//Aprobado:Debes usar el operador /= para cada variable.
+//Aprobado:No debes modificar el código sobre el comentario especificado.
+let a = 48;
+let b = 108;
+let c = 33;
+
+// Cambia solo el código debajo de esta línea
+a /= 12;
+b /=  4;
+c /=  11;
